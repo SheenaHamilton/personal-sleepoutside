d0a4ad411b85846cbe7be77e4702f365489104a6
sheenahamilton <sheenhamilton@gmail.com>
2024-05-05 02:12:49 -0600

    fix the cart. Add Data ID to function

--- src/js/product.js
+++ src/js/product.js
@@ -3,13 +3,13 @@ import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
-function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+function addProductToCart(dataid, product) {
+  setLocalStorage(dataid, product);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  addProductToCart(e.target.dataset.id,product);
 }
 
 // add listener to Add to Cart button
