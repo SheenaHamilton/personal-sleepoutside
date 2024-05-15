040f53f6d2a20fda30dcc7cbcf81d0b14cdde64e
sheenahamilton <sheenhamilton@gmail.com>
2024-05-15 11:42:51 -0600

    SH--Individual 3 Assignment

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -2,8 +2,8 @@ import {renderListWithTemplate} from './utils.mjs';
 
 export function productCardTemplate(product) {
     return `<li class="product-card">
-            <a href="product_pages/?product=${product.Id}">
-            <img src="${product.Image}" alt="Image of ${product.Name}"/>
+            <a href="../product_pages/index.html?product=${product.Id}">
+            <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}"/>
             <h3 class="card__brand">${product.Brand.Name}</h3>
             <h2 class="card__name">${product.Name}</h2>
             <p class="product-card__price">${product.FinalPrice}</p></a>
@@ -18,17 +18,10 @@ export default class ProductListing {
     }
 
     async init() {
-        const productList = await this.dataSource.getData();
-        const filteredProducts = productList.filter(this.filterList);
-        this.renderList(filteredProducts);
+        const productList = await this.dataSource.getData(this.category);
+        this.renderList(productList);
     }
 
-    filterList(listitem) {
-        const includeTents = ['880RR','985RF','985PR','344YJ'];
-        if (includeTents.includes(listitem.Id)) {
-            return listitem;
-        }
-    }
 
     renderList(list) {
         renderListWithTemplate (productCardTemplate,this.listElement,list);
